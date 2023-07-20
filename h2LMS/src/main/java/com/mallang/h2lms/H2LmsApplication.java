25435abd572a5ee59b32a7ecadf30b2b2416d6aa
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-07-21 03:30:34 +0900

    Feat : h2LMS_개발환경구축 #2

---
+++ h2LMS/src/main/java/com/mallang/h2lms/H2LmsApplication.java
@@ -0,0 +1,13 @@
+package com.mallang.h2lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2LmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2LmsApplication.class, args);
+    }
+
+}
