03888d921dbce018f39c4af7b1baeb0d34a26d36
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-07-26 12:20:41 +0900

    Feat : PersistenceContext_개발환경구축 #20 - 프로젝트파일생성 - .gitignore설정

---
+++ PersistenceContext/src/main/java/com/mallang/persistencecontext/PersistenceContextApplication.java
@@ -0,0 +1,13 @@
+package com.mallang.persistencecontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenceContextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenceContextApplication.class, args);
+    }
+
+}
