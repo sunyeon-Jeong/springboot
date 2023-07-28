698f6c62e65c18d1d0bd3d1a019cd2cbdcc7382c
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-07-28 15:18:08 +0900

    Feat : HelloMVC_개발환경구축 #31 - 프로젝트생성 - 패키지정리 - .gitignore설계

---
+++ HelloMVC/src/main/java/com/mallang/hellomvc/HelloMvcApplication.java
@@ -0,0 +1,13 @@
+package com.mallang.hellomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMvcApplication.class, args);
+    }
+
+}
