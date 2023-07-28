7877a176648a11ae54543711ba1101595d7d982b
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-07-28 17:30:40 +0900

    Comment : HelloMVC_HelloResponseController #38 - HelloResponseController.java -> 주석추가

--- HelloMVC/src/main/java/com/mallang/hellomvc/controller/HelloResponseController.java
+++ HelloMVC/src/main/java/com/mallang/hellomvc/controller/HelloResponseController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/response")
+@RequestMapping("/response") // 전체 url ->  /response로 시작
 public class HelloResponseController {
-    // 클래스 변수
+    // 방문자수 증가시키는 클래스 변수
     private static long visitCount = 0;
 
     // 정적웹페이지
@@ -32,6 +32,7 @@ public class HelloResponseController {
     @ResponseBody
     @GetMapping("/body/html")
     public String helloStringHTML() {
+        // return값이 html -> 바로 적용되어 나옴
         return "<!DOCTYPE html>" +
                 "<html>" +
                 "<head><meta charset=\"UTF-8\"><title>By @ResponseBody</title></head>" +
@@ -40,10 +41,14 @@ public class HelloResponseController {
     }
 
     // 동적웹페이지
+    // Thymeleaf방식
+    // Model <- Client가 원하는 값, hello-visit.html을 반환
     @GetMapping("/html/dynamic")
     public String helloHtmlFile(Model model) {
         visitCount++;
+        // hello-visit에 visits부분에 visitCount 삽입
         model.addAttribute("visits", visitCount);
+        // Controller는 Model을 이용해 데이터를 가져옴, View에 데이터를 넘김
         return "hello-visit";
     }
 
@@ -53,12 +58,14 @@ public class HelloResponseController {
     @ResponseBody
     @GetMapping("/json/string")
     public String helloStringJson() {
+        // 콘솔 <body></body>에 json형태로 찍힘
         return "{\"name\":\"말랑\",\"age\":20}";
     }
 
     @ResponseBody
     @GetMapping("/json/class")
     public Star helloJson() {
+        // star 객체가 json 형태로 반환됨
         return new Star("말랑", 22);
     }
 }
