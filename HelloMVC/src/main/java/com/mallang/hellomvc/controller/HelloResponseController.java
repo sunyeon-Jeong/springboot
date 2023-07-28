5d087eee415c725dcbfc630a24bdbaf122208881
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-07-28 17:12:51 +0900

    Refactor : HelloMVC_HelloResponseController #36 - HelloResponseController.java Refactoring

--- HelloMVC/src/main/java/com/mallang/hellomvc/controller/HelloResponseController.java
+++ HelloMVC/src/main/java/com/mallang/hellomvc/controller/HelloResponseController.java
@@ -1,6 +1,7 @@
 package com.mallang.hellomvc.controller;
 
 import com.mallang.hellomvc.entity.Star;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
