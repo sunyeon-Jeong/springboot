df22d40a2eab7018b1398850f0e6b61c6c10c5cc
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-07-31 16:33:57 +0900

    Feat : mallangMEMO_Application파일생성 #50 - MallangMemoApplication.java

---
+++ mallangMEMO/src/main/java/com/mallang/mallangmemo/MallangMemoApplication.java
@@ -0,0 +1,11 @@
+package com.mallang.mallangmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallangMemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallangMemoApplication.class, args);
+    }
+}
