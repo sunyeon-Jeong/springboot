0dc7076508077ddb83399c992f616eef5554b766
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-02 16:10:17 +0900

    Feat : mallangMEMO_Memo생성설계 #59 - Controller -> createMemo생성

--- mallangMEMO/src/main/java/com/mallang/mallangmemo/controller/MemoController.java
+++ mallangMEMO/src/main/java/com/mallang/mallangmemo/controller/MemoController.java
@@ -1,16 +1,28 @@
 package com.mallang.mallangmemo.controller;
 
+import com.mallang.mallangmemo.dto.MemoRequestDto;
+import com.mallang.mallangmemo.entity.Memo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController // @Controller + @ResponseBody -> Json형태의 데이터반환
 @RequiredArgsConstructor // final, @Notnull -> 생성자 자동생성
 public class MemoController {
-    // html 반환
+    // service단 객체 불러옴
+    private final MemoService memoService;
+
+    // 메인페이지 - html 반환
     @GetMapping("/")
     public ModelAndView home() {
         return new ModelAndView("index");
+    }
+
+    // Memo 생성하기
+    @PostMapping("/api/memos")
+    public Memo createMemo(MemoRequestDto memoRequestDto) {
+        return memoService.createMemo(memoRequestDto);
     }
 }
