974bcd1d4cd8c7d17c053eca6ed9e681573128b3
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-08 15:56:08 +0900

    Feat : mallangLOG(Level1)_Entity설계 #72 - Post.java

--- mallangLOG/src/main/java/com/mallang/mallanglog/entity/Post.java
+++ mallangLOG/src/main/java/com/mallang/mallanglog/entity/Post.java
@@ -1,7 +1,28 @@
 package com.mallang.mallanglog.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Entity
+@Getter
+@RequiredArgsConstructor // final, @Notnull 필드 -> 생성자생성
 public class Post extends Timestamped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String content;
+
 }
