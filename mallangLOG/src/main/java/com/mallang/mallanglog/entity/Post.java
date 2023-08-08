c7bf9c97b754c56c0642313b215b9616f4b918c8
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-08 16:10:26 +0900

    Feat : mallangLOG(Level1)_Dto설계 #74 - Post.java -> 정적팩토리메서드

--- mallangLOG/src/main/java/com/mallang/mallanglog/entity/Post.java
+++ mallangLOG/src/main/java/com/mallang/mallanglog/entity/Post.java
@@ -1,6 +1,8 @@
 package com.mallang.mallanglog.entity;
 
+import com.mallang.mallanglog.dto.PostRequestDto;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -24,5 +26,24 @@ public class Post extends Timestamped {
 
     @Column(nullable = false)
     private String content;
+
+    // Client Request 값 -> Dto에서 받아와서 -> 덮어씌움
+    @Builder // 빌더패턴 : 생성자를 통해 값을 받음 (-> 정적팩토리메서드)
+    private Post(String title, String username, String password, String content) {
+        this.title = title;
+        this.username = username;
+        this.password = password;
+        this.content = content;
+    }
+
+    // 정적팩토리메서드
+    public static Post of(PostRequestDto postRequestDto) {
+        return Post.builder()
+                .title(postRequestDto.getTitle())
+                .username(postRequestDto.getUsername())
+                .password(postRequestDto.getPassword())
+                .content(postRequestDto.getContent())
+                .build();
+    }
 
 }
