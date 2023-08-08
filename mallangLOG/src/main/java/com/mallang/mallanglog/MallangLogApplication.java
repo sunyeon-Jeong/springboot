3af2aed9368c904c1a8fca2e6f6e777749520048
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-08 15:24:17 +0900

    Feat : mallangLOG(Level1)_Entity설계 #72 - Timestamped.java - MallangLOGApplication.java

--- mallangLOG/src/main/java/com/mallang/mallanglog/MallangLogApplication.java
+++ mallangLOG/src/main/java/com/mallang/mallanglog/MallangLogApplication.java
@@ -2,8 +2,10 @@ package com.mallang.mallanglog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // @EntityListeners와 연결
 public class MallangLogApplication {
 
     public static void main(String[] args) {
