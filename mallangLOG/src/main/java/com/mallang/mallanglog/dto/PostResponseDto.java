6a5a1fcfb0ff04112287c2a8a7a8f33e3f4ac394
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-08 16:15:03 +0900

    Feat : mallangLOG(Level1)_Dto설계 #74 - PostResponseDto.java

--- mallangLOG/src/main/java/com/mallang/mallanglog/dto/PostResponseDto.java
+++ mallangLOG/src/main/java/com/mallang/mallanglog/dto/PostResponseDto.java
@@ -2,6 +2,21 @@ package com.mallang.mallanglog.dto;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class PostResponseDto {
+
+    private Long id;
+
+    private String title;
+
+    private String username;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime modifiedAt;
+
 }
