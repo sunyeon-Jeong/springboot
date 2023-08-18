ba3f5aa600044b7bae48f7d665c7b141db1fe7bd
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-18 18:55:58 +0900

    Setting : mallangShopBeta_개발환경구축 #90 - 프로젝트생성 - .gitignore설정 - application.properties설정 - 디렉토리정리

---
+++ mallangShopBeta/src/main/java/com/mallang/mallangshopbeta/MallangShopBetaApplication.java
@@ -0,0 +1,13 @@
+package com.mallang.mallangshopbeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallangShopBetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallangShopBetaApplication.class, args);
+    }
+
+}
