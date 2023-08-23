9f9773aca1ba5c72778f268ef19058ee26200353
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-23 21:18:14 +0900

    Feat : mallangShopBeta_Product설계 #101 - dto > ProductMypriceRequestDto.java : 이름 리팩토링

--- mallangShopBeta/src/main/java/com/mallang/mallangshopbeta/dto/ProductMypriceRequestDto.java
+++ mallangShopBeta/src/main/java/com/mallang/mallangshopbeta/dto/ProductMypriceRequestDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductMyPriceRequestDto {
+public class ProductMypriceRequestDto {
 
     private int myprice;
 
