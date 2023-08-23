d2f1ea349c589fd42f0cf0e7c42463535a3a8a1b
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-23 21:14:49 +0900

    Feat : mallangShopBeta_Product설계 #101 - dto > ProductResponseDto.java

---
+++ mallangShopBeta/src/main/java/com/mallang/mallangshopbeta/dto/ProductResponseDto.java
@@ -0,0 +1,47 @@
+package com.mallang.mallangshopbeta.dto;
+
+import com.mallang.mallangshopbeta.entity.Product;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor // 파라미터없는 기본생성자
+public class ProductResponseDto {
+
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private String image;
+
+    private int lprice;
+
+    private int myprice;
+
+    // 생성자
+    @Builder
+    private ProductResponseDto(Long id, String title, String link, String image, int lprice, int myprice) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.image = image;
+        this.lprice = lprice;
+        this.myprice = myprice;
+    }
+
+    // 정적팩토리메서드
+    public static ProductResponseDto of(Product product) {
+        return ProductResponseDto.builder()
+                .id(product.getId())
+                .title(product.getTitle())
+                .link(product.getLink())
+                .image(product.getImage())
+                .lprice(product.getLprice())
+                .myprice(product.getMyprice())
+                .build();
+    }
+
+}
