0ce4ea105650e1a520e21fede7208dd41f0f9fae
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-26 15:49:17 +0900

    Feat : mallangShop_기초코드설계 #113 - dto > ProductResponseDto.java, ProductRequestDto.java, ProductMypriceRequestDto.java

---
+++ mallangShop/src/main/java/com/mallang/mallangshop/dto/ProductMypriceRequestDto.java
@@ -0,0 +1,14 @@
+package com.mallang.mallangshop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductMypriceRequestDto {
+
+    private int myprice;
+
+}
