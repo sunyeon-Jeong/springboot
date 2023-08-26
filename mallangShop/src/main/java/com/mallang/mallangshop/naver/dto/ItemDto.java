cac22930a26688df5e4af5dea309920242f32842
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-26 16:00:35 +0900

    Feat : mallangShop_기초코드설계 #113 - naver - org.json설계

---
+++ mallangShop/src/main/java/com/mallang/mallangshop/naver/dto/ItemDto.java
@@ -0,0 +1,39 @@
+package com.mallang.mallangshop.naver.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.json.JSONObject;
+
+@Getter
+@NoArgsConstructor
+public class ItemDto {
+
+    private String title;
+
+    private String link;
+
+    private String image;
+
+    private int lprice;
+
+    // 생성자
+    @Builder
+    private ItemDto(String title, String link, String image, int lprice) {
+        this.title = title;
+        this.link = link;
+        this.image = image;
+        this.lprice = lprice;
+    }
+
+    // 정적팩토리메서드
+    public static ItemDto of(JSONObject itemJson) {
+        return ItemDto.builder()
+                .title(itemJson.getString("title"))
+                .link(itemJson.getString("link"))
+                .image(itemJson.getString("image"))
+                .lprice(itemJson.getInt("lprice"))
+                .build();
+    }
+
+}
