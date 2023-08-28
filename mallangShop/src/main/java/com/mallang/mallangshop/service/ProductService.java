0e748506a12e3559cb49a29be7fdaa0be0bb6da0
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-28 21:31:00 +0900

    Refactor : mallangShop_Bean최종설계 #121 - Lombok -> @RequriedArgsConstructor ; @Autowired + 생성자 생략

--- mallangShop/src/main/java/com/mallang/mallangshop/service/ProductService.java
+++ mallangShop/src/main/java/com/mallang/mallangshop/service/ProductService.java
@@ -5,23 +5,18 @@ import com.mallang.mallangshop.dto.ProductRequestDto;
 import com.mallang.mallangshop.dto.ProductResponseDto;
 import com.mallang.mallangshop.entity.Product;
 import com.mallang.mallangshop.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.sql.SQLException;
 import java.util.List;
 
 @Component // Bean 등록
+@RequiredArgsConstructor
 public class ProductService {
 
     // 객체중복생성 해결 -> 멤버변수 선언
     private final ProductRepository productRepository;
-
-    // 생성자
-    @Autowired
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
 
     // 관심상품 등록
     public ProductResponseDto createProduct(ProductRequestDto productRequestDto) throws SQLException {
