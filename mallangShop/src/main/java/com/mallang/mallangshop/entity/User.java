d0676f236cc0696260c4fcd7e25514fdbdf03dc0
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-31 14:13:24 +0900

    Feat : mallangShop_User설계 #129 - entity > User.java

---
+++ mallangShop/src/main/java/com/mallang/mallangshop/entity/User.java
@@ -0,0 +1,38 @@
+package com.mallang.mallangshop.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "users") // 예약어 -> users로 변경
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    // 생성자
+    public User(String username, String password, String email, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+
+}
