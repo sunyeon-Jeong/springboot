36cad804507fee5ff9424a3858157c8a1ff47532
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-31 14:33:59 +0900

    Feat : mallangShop_UserDto설계 #132 - dto > SignupRequestDto.java

---
+++ mallangShop/src/main/java/com/mallang/mallangshop/dto/SignupRequestDto.java
@@ -0,0 +1,20 @@
+package com.mallang.mallangshop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SignupRequestDto {
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private boolean admin = false;
+
+    private String adminToken = "";
+
+}
