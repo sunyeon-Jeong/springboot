ec6f1cbba6bfa9e62fe06a71f0162bafa05373ff
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-08-31 16:41:07 +0900

    Feat : mallangShop_Signup구현 #134 - 회원가입설계

--- mallangShop/src/main/java/com/mallang/mallangshop/controller/UserController.java
+++ mallangShop/src/main/java/com/mallang/mallangshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.mallang.mallangshop.controller;
 
 import com.mallang.mallangshop.dto.SignupRequestDto;
+import com.mallang.mallangshop.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- mallangShop/src/main/java/com/mallang/mallangshop/repository/UserRepository.java
+++ mallangShop/src/main/java/com/mallang/mallangshop/repository/UserRepository.java
@@ -3,5 +3,11 @@ package com.mallang.mallangshop.repository;
 import com.mallang.mallangshop.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 회원중복확인
+    Optional<User> findByUsername(String username);
+
 }
