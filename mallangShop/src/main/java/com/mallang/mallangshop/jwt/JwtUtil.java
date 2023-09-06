f587f537eabc970c4d03ab05e5ed532e718043c2
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-09-06 13:56:22 +0900

    Feat : mallangShop_JWT설계 #138 - JwtUtil -> Token 생성 필요값 설계

---
+++ mallangShop/src/main/java/com/mallang/mallangshop/jwt/JwtUtil.java
@@ -0,0 +1,53 @@
+package com.mallang.mallangshop.jwt;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtUtil {
+
+    /* 1. JWT Token 생성 필요값 */
+    // Header Key 값
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    // 사용자 권한값 Key
+    public static final String AUTHORIZATION_KEY = "auth";
+
+    // JWT Token 인증방식 (Bearer + JWT Token 함께 전송)
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    // JWT Token 만료시간
+    private static final long TOKEN_TIME = 60 * 60 * 1000L; // ms기준 -> 1시간
+
+    // application.properties -> JWT Secret Key 불러옴
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    // JWT Token 생성 및 검증 Key 저장용도
+    private Key key;
+
+    // JWT Token 생성(사인)알고리즘
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    // Spring Bean 초기화 -> 실행되는 메서드
+    @PostConstruct
+    public void init() {
+        // secretKey -> 디코딩 -> byte배열에 담음
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+
+        // 디코딩 한 byte배열 -> JWT Token 서명 및 검증에 필요한 key 생성
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+
+}
