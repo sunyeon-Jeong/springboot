dfcd2dce00034827a28549bc8f614c10e53ff9b7
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-09-23 15:46:13 +0900

    Feat : mallangLOG(Level2)_User설계 #157 - dto > SignupRequestDto.java

--- mallangLOG/src/main/java/com/mallang/mallanglog/dto/SignupRequestDto.java
+++ mallangLOG/src/main/java/com/mallang/mallanglog/dto/SignupRequestDto.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @Getter
 public class SignupRequestDto {
 
+    // build.gradle -> validation 의존성추가
     @Size(min = 4, max = 10, message = "최소 4자, 최대 10자까지 설정 가능합니다")
     @Pattern(regexp = "[a-z0-9]*$", message = "알파벳소문자 및 숫자로만 설정 가능합니다")
     private String username;
