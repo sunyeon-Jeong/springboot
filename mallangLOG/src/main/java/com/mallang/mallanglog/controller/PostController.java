6762e2329b76e68712f148df0507c766bef07ecd
sunyeon-Jeong <sunyeon4012@gmail.com>
2023-10-04 11:46:25 +0900

    Feat : mallangLOG(Level2)_게시글작성JWT인가설계 #169 - PostController.java ; httpServletRequest 추가

--- mallangLOG/src/main/java/com/mallang/mallanglog/controller/PostController.java
+++ mallangLOG/src/main/java/com/mallang/mallanglog/controller/PostController.java
@@ -3,6 +3,7 @@ package com.mallang.mallanglog.controller;
 import com.mallang.mallanglog.dto.PostRequestDto;
 import com.mallang.mallanglog.dto.PostResponseDto;
 import com.mallang.mallanglog.service.PostService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,36 +18,45 @@ public class PostController {
 
     // Post 생성
     @PostMapping("/post")
-    public PostResponseDto createPost(@RequestBody PostRequestDto postRequestDto) {
-        return postService.createPost(postRequestDto);
+    public PostResponseDto createPost(@RequestBody PostRequestDto postRequestDto,
+                                      HttpServletRequest httpServletRequest) {
+
+        return postService.createPost(postRequestDto, httpServletRequest);
+
     }
-        // 느슨한결합 : Entity 바로 반환 X, Dto에 담아 반환 !
-        // @RequestBody : Client 입력값 -> HTTP Body에 JSON형태로 지정객체에 담아옴
 
     // Post 전체조회
     @GetMapping("/posts")
     public List<PostResponseDto> getPosts() {
+
         return postService.getPosts();
+
     }
 
     // Post 선택조회
     @GetMapping("/post/{post-id}")
     public PostResponseDto getSelectedPost(@PathVariable(name = "post-id") Long postId) {
+
         return postService.getSelectedPost(postId);
+
     }
 
     // Post 수정
     @PutMapping("/post/{post-id}")
     public PostResponseDto updatePost(@PathVariable(name="post-id") Long postId,
                                       @RequestBody PostRequestDto postRequestDto) {
+
         return postService.updatePost(postId, postRequestDto);
+
     }
 
     // Post 삭제
     @DeleteMapping("/post/{post-id}")
     public Map<Integer, String> deletePost(@PathVariable(name="post-id") Long postId,
                                           @RequestBody PostRequestDto postRequestDto) {
+
         return postService.deletePost(postId, postRequestDto);
+
     }
 
 }
